c35f7cd78186a6f0be5274970744e42387233ed8
Nihal Abdul Khader MP <nihalmp45@gmail.com>
2022-12-24 20:45:22 +0530

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <div>Movie App</div>
-      <div>©2022, Movie, Inc. or its affiliates</div>
+      <div className="footer-details">Movie App</div>
+      <div className="footer-details">©2022, Movie, Inc. or its affiliates</div>
     </div>
   );
 };
